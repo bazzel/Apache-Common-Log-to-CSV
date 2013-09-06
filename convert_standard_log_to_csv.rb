12aa79f4e70b63683f6b46ca9552c82ab6256e31
Patrick Baselier <patrick@kabisa.nl>
2013-09-06 11:37:05 +0200

    Make it work (a little better)

--- convert_standard_log_to_csv.rb
+++ convert_standard_log_to_csv.rb
@@ -1,57 +1,44 @@
-require 'rubygems'
 require 'csv'
+require 'pathname'
 require 'apachelogregex'
 
-# Requires the following gem:
-# gem install apachelogregex
-
 # find all of the files in the directory
-file_dir = "./CLF/"
-files = Dir.new(file_dir).entries
-files.delete(".")
-files.delete("..")
-files.delete(".DS_Store")
+# TODO: Update this path if if needed.
+file_dir = '/Users/patrick/Downloads/var/log/apache2/'
+
+`gunzip #{file_dir}*.gz`
+log_files = Pathname.new(file_dir).children.select { |c| "#{c.basename}".match('log(\.\d+)?$') }
 
 # go through each one
-files.each do |filename|
-  filename = file_dir + filename
-  
-  # handle line endings
-  system("dos2unix #{filename}")
-  
-  #prep the output file
-  outfile = File.open(filename + ".csv", 'a')
-  
-  # write headers
-  csv = []
-  CSV::Writer.generate(outfile) do |csv|
-    csv << ['Client IP','IdentityCheck','HTTPAuthUser','RequestFinishedAt','Request','Status','Size','Referer','UserAgent']
-  end
-  
-  # read the file in line by line
-  file = File.new(filename, "r")
-  while (line = file.gets)
-    # write row to the csv file
-    csv = []
-    CSV::Writer.generate(outfile) do |csv|
-      # parse the Apache Common Log Format
-      
-      # Define the log file format.
-      # This information is defined in you Apache log file with the LogFormat directive
-      format = '%h %l %u %t \"%r\" %>s %b \"%{Referer}i\" \"%{User-Agent}i\"'
+log_files.each do |pathname|
+  begin
+    # handle line endings
+    system("dos2unix #{pathname}")
 
-      # Initialize the parser
-      parser = ApacheLogRegex.new(format)
+    csv_filename = "#{pathname}.csv"
+    CSV.open(csv_filename, 'wb') do |csv|
+      csv << ['Remote host','Remote logname','Remote user','Request Time','Request','Status','Bytes sent','Referer','UserAgent', 'Duration (e-6s)']
 
-      # Get the log line as a Hash
-      l = parser.parse(line)
-      # => {"%r"=>"GET /blog/index.xml HTTP/1.1", "%h"=>"87.18.183.252", "%>s"=>"302", "%t"=>"[13/Aug/2008:00:50:49 -0700]", "%{User-Agent}i"=>"Feedreader 3.13 (Powered by Newsbrain)", "%u"=>"-", "%{Referer}i"=>"-", "%b"=>"527", "%l"=>"-"}
-      
-      # parse that to CSV
-      csv << [ l["%h"], l["%l"], l["%u"], l["%t"], l["%r"], l["%>s"], l["%b"], l["%{Referer}i"], l["%{User-Agent}i"] ]
+      pathname.each_line do |line|
+        # Define the log file format.
+        # This information is defined in you Apache log file with the LogFormat directive
+        # /etc/apache2/apache2.conf
+        format = '"%v:%p %h %l %u %t \"%r\" %>s %O \"%{Referer}i\" \"%{User-Agent}i\" %D"'
+
+        # Initialize the parser
+        parser = ApacheLogRegex.new(format)
+
+        # Get the log line as a Hash
+        l = parser.parse(line)
+        # => {"\"%v:%p"=>"oss-production.philips.com:80", "%h"=>"80.239.228.47", "%l"=>"-", "%u"=>"-", "%t"=>"[04/Sep/2013:06:32:30 +0000]", "%r"=>"GET /assets/integration/1.0/oss-hld.js HTTP/1.1", "%>s"=>"200", "%O"=>"3720", "%{Referer}i"=>"http://www.click-licht.de/Nachttischleuchten-Nachttischlampen_s2", "%{User-Agent}i"=>"Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Win64; x64; Trident/5.0; der GWH Frankfurt)", "%D\""=>"1164"}
+
+        # parse that to CSV
+        csv << [ l["%h"], l["%l"], l["%u"], l["%t"], l["%r"], l["%>s"], l["%b"], l["%{Referer}i"], l["%{User-Agent}i"], l["%D\""] ]
+      end
     end
+  rescue
+    puts "[ERROR] Skipping %s, due to %s" % [pathname, $!]
+    Pathname.new(csv_filename).delete
   end
-  
-  file.close
-  outfile.close
+
 end
